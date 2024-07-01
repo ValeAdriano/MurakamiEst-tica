41bd003cd2714734fed8991f30665434ca6fdf08
Adriano Vale <acsvalefilho@gmail.com>
2024-07-01 20:26:19 -0300

    .

--- Murakami/src/app/app.routes.ts
+++ Murakami/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'header', component: HeaderComponent},
+    {path: 'footer', component: FooterComponent},
+    {path: 'home', component: HomeComponent},
+];
+
+
